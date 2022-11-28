76e73e25cf723d227b9065763afb53520e41e1e5
wasim4491 <52147807+wasim4491@users.noreply.github.com>
2022-11-28 10:11:58 +0530

    Time: 38 ms (74.17%), Space: 31.2 MB (51.97%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
-    int sum(vector<int>& nums, int l, int r){
-        int sums = 0;
-        for(int i=l;i<=r;i++){
-            sums += nums[i];
-        }
-        return sums;
-    }
     int pivotIndex(vector<int>& nums) {
-     //   int l = 0, r = nums.size()-1;
-        int n = nums.size()-1;
-      //  int mid = l + (r - l)/2;
-        int ans = -1;
+        int leftsum = 0, rightsum = 0,ans = -1,flag = 1,n=nums.size();
         for(int i=0;i<nums.size();i++){
-            int sum1 = sum(nums,0,i-1);
-            int sum2 = sum(nums,i+1,n);
-            if(sum1 == sum2){
+            leftsum += nums[i];
+        }
+        for(int i=0;i<nums.size();i++){
+            leftsum = leftsum - nums[i];
+            if(rightsum == leftsum){
                 ans = i;
+                flag = 0;
                 break;
             }
+            rightsum += nums[i];
+            
         }
-        // while(l <= r){
-        //     mid = l + (r - l)/2;
-        //     int sum1 = sum(nums,0,mid-1);
-        //     int sum2 = sum(nums,mid+1,n);
-        //     if(sum1 == sum2){
-        //         ans = mid;
-        //         break;
-        //     }
-        //     else if(sum1 > sum2){
-        //         r = mid - 1;
-        //     }
-        //     else{
-        //         l = mid + 1;
-        //     }
-        // }
+        if(flag == 1){
+            rightsum += nums[n-1];
+            if(rightsum == leftsum){
+                ans = n-1;
+            }
+        }
+        
+        cout << rightsum << " " <<leftsum;
         return ans;
     }
 };
