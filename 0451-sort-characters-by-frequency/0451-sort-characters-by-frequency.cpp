8f4573e9b4399af46785769fc73d764c08d80ab3
wasim4491 <52147807+wasim4491@users.noreply.github.com>
2022-12-04 05:57:02 +0530

    Time: 43 ms (12.93%), Space: 8.5 MB (67.27%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,3 +1,9 @@
+class Node{
+    public:
+    char pk;
+    int count;
+};
+
 class Solution {
 public:
     string frequencySort(string s) {
@@ -10,17 +16,24 @@ public:
                 mp[s[i]]++;
             }
         }
+        vector<Node> v3;
+        
         vector<char> v1;
         vector<int> v2;
         for(auto i : mp){
             v1.push_back(i.first);
             v2.push_back(i.second);
+            Node* obj = new Node();
+            obj->pk = i.first;
+            obj->count = i.second;
+            v3.push_back(*obj);
         }
         
-        for(int i=0;i<v2.size();i++){
-            cout << v1[i] << " " << v2[i] <<endl;
-        }
-        int k = v2.size();
+        sort(v3.begin(),v3.end(), [ ]( const Node& lhs, const Node& rhs )
+{
+   return lhs.count > rhs.count;
+} );
+       /* int k = v2.size();
         for(int i=0;i<k;i++){
             for(int j=i;j<k;j++){
                 if(v2[i] < v2[j]){
@@ -33,11 +46,13 @@ public:
             cout << v1[i] << " " << v2[i] <<endl;
         }
         cout << endl;
+        */
+        int k = v3.size();
         string res = "";
         for(int i=0;i<k;i++){
-            while(v2[i]){
-            res.push_back(v1[i]);
-                v2[i]--;
+            while(v3[i].count){
+            res.push_back(v3[i].pk);
+                v3[i].count--;
             }
         }
         return res;
