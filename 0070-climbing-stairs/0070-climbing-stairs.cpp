d76d12c7b02953df35281b7fec095dbb28a25449
wasim4491 <52147807+wasim4491@users.noreply.github.com>
2023-05-09 07:10:31 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (21.71%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int ans(int n, vector<int>& dp){
+        if(n <= 1){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = ans(n-1, dp) + ans(n-2, dp) ;
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return ans(n,dp);
+    }
+};
