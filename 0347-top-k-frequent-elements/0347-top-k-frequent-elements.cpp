be903e7f8816076c0ab067e36cefc000bcf18f1d
wasim4491 <52147807+wasim4491@users.noreply.github.com>
2023-05-22 07:29:33 +0530

    Time: 20 ms (43.48%), Space: 13.8 MB (42.78%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -5,35 +5,16 @@ bool cmp(pair<int,int> a, pair<int,int> b){
 class Solution {
 public:
     
-    // struct boolcmpoperator{
-    //     bool cmp(int i, int j, unordered_map<int,int> ansmap){
-    //         if(ansmap[i] > ansmap[j]){
-    //             return true;
-    //         }
-    //         return false;
-    //     }
-    // };
-    
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> ansmap;
         for(int i=0; i<nums.size(); i++){
-            // if(ansmap.find(nums[i]) == ansmap.end()){
-            //     ansmap[nums[i]] = 1;
-            // }
-            // else{
-            //     ansmap[nums[i]]++;
-            // }
             ansmap[nums[i]]++;
         }
         
         vector<pair<int,int>> res;
         for(auto i : ansmap){
-            //cout << i.first << endl;
             res.push_back({i.first, i.second});
         }
-        // for(int i=0;i<res.size();i++){
-        //     cout << res[i].first << " " << res[i].second << endl;
-        // }
         sort(res.begin(), res.end(), cmp);
         
         vector<int> result;
