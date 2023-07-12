f77cd69429c0e75fae8dfc86520a0bfce5f76b58
wasim4491 <52147807+wasim4491@users.noreply.github.com>
2023-07-13 05:00:29 +0530

    Time: 15 ms (84.24%), Space: 13.6 MB (55.17%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,27 +1,32 @@
-bool cmp(pair<int,int> a, pair<int,int> b){
+bool cmp(pair<int, int> a, pair<int, int> b){
         return a.second > b.second;
     }
-
 class Solution {
 public:
     
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> ansmap;
+        unordered_map<int, int> map;
         for(int i=0; i<nums.size(); i++){
-            ansmap[nums[i]]++;
+            if(map.find(nums[i]) == map.end()){
+                map[nums[i]] = 1;
+            }
+            else{
+                map[nums[i]]++;
+            }
         }
         
-        vector<pair<int,int>> res;
-        for(auto i : ansmap){
-            res.push_back({i.first, i.second});
+        vector<pair<int, int>> result;
+        for(auto it : map){
+            result.push_back({it.first, it.second});
         }
-        sort(res.begin(), res.end(), cmp);
         
-        vector<int> result;
+        sort(result.begin(), result.end(), cmp);
         
-        for(int i=0;i<k;i++){
-            result.push_back(res[i].first);
+        vector<int> ans;
+        for(int i=0; i<k; i++){
+            ans.push_back(result[i].first);
         }
-        return result;
+        
+        return ans;
     }
 };
