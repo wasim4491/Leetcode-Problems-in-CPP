509a7b07a0c828e06ffae47121dae84e878bf05d
wasim4491 <52147807+wasim4491@users.noreply.github.com>
2023-08-08 14:48:42 +0530

    Time: 16 ms (62.11%), Space: 13.5 MB (11.43%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        unordered_map<int, bool> mapi;
-        unordered_map<int, bool> mapj;
-        
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool flag1 = false, flag2 = false;
+        for(int i=0; i<m; i++){
+            if(matrix[i][0] == 0){
+                flag1 = true;
+            }
+        }
+        for(int j=0; j<n; j++){
+            if(matrix[0][j] == 0){
+                flag2 = true;
+            }
+        }
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
                 if(matrix[i][j] == 0){
-                    if(mapi.find(i) == mapi.end()){
-                        mapi[i] = true;
-                    }
-                    if(mapj.find(j) == mapj.end()){
-                        mapj[j] = true;
-                    }
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for(int i=0; i<matrix.size(); i++){
-            for(int j=0; j<matrix[0].size(); j++){
-                if(mapi[i] == true || mapj[j] == true){
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
+            }
+        }
+        if(flag1){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        if(flag2){
+            for(int j=0; j<n; j++){
+                matrix[0][j] = 0;
             }
         }
     }
