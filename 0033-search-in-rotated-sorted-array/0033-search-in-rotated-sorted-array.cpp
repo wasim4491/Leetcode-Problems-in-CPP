235e71b76df4b00a5039ab384223cbb016374a0f
wasim4491 <52147807+wasim4491@users.noreply.github.com>
2023-08-08 08:29:59 +0530

    Time: 7 ms (28.91%), Space: 11.1 MB (45.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
-    int pivot(vector<int> arr){
-        int n = arr.size();
-        int s = 0;
-        int e = n - 1;
-        int mid ;
-        while(s < e){
-            mid = s + (e-s)/2;
-            if(arr[mid] >= arr[0]){
-                s = mid + 1;
+    int pivot(vector<int> &arr){
+        int l = 0, r = arr.size() - 1;
+        int mid;
+        while(l < r){
+            mid = l + (r - l)/2;
+            if(arr[mid] < arr[r]){
+                r = mid;
             }
-            else{
-                e = mid;
+            else {
+                l = mid + 1;
             }
         }
-        return s;
+        return l;
     }
-    int search(vector<int>& arr, int target) {
-        int piv = pivot(arr);
-        int n = arr.size();
-        int s=0,e=n-1,mid;
-        if(arr[piv] == target){
-            return piv;
-        }
-        if(arr[piv] < target && target <= arr[e]){
-            s = piv + 1;
-        }
-        else{
-            e = piv - 1;
-        }
-        while(s <= e){
-            mid = s + (e-s)/2;
-            if(arr[mid] == target){
+    int bs(vector<int> &nums, int start, int end, int target){
+        int l = start, r = end;
+        int mid;
+        while(l <= r){
+            mid = l + (r-l)/2;
+            if(nums[mid] == target){
                 return mid;
             }
-            else if(arr[mid] > target){
-                e = mid - 1;
+            else if(nums[mid] > target){
+                r = mid - 1;
             }
             else{
-                s = mid + 1;
+                l = mid + 1;
             }
         }
         return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        int piv = pivot(nums);
+        if(nums[piv] == target){
+            return piv;
+        }
+        int s1 = bs(nums, 0, piv-1, target);
+        if(s1 != -1){
+            return s1;
+        }
+        int s2 = bs(nums, piv, nums.size() - 1, target);
+        return s2;
     }
 };
