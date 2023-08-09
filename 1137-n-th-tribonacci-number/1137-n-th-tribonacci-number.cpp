d039a7e3e3e4eed015fbbbd633904d9ac50d2b72
wasim4491 <52147807+wasim4491@users.noreply.github.com>
2023-08-10 05:08:08 +0530

    Time: 3 ms (30.64%), Space: 6.2 MB (19.92%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int helper(int n, vector<int> &dp){
+        if(n == 0){
+            return 0;
+        }
+        if(n <= 2){
+            return 1;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        return dp[n] = helper(n-1, dp) + helper(n-2, dp) + helper(n-3, dp);
+    }
+    int tribonacci(int n) {
+        vector<int> dp(n+1, -1);
+        return helper(n, dp);
+    }
+};
