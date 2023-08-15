2ac24b4e1cab9f150e5afe3535f817d57fd4b62c
wasim4491 <52147807+wasim4491@users.noreply.github.com>
2023-08-15 12:40:23 +0530

    Time: 121 ms (84.88%), Space: 24 MB (71.52%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,28 +3,27 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        set<vector<int>> mainarr;
-        for(int i=0; i<n; i++){
-            int l = i+1, r = n - 1;
-            while(l < r){
-                vector<int> subarr;
-                if(nums[i] + nums[l] + nums[r] < 0){
-                    l++;
-                }
-                else if(nums[i] + nums[l] + nums[r] > 0){
-                    r--;
-                }
-                else{
-                    subarr.push_back(nums[l]);
-                    subarr.push_back(nums[r]);
-                    subarr.push_back(nums[i]);
-                    mainarr.insert(subarr);
-                    l++;
-                    r--;
+        vector<vector<int>> ans;
+        
+        for (int i = 0; i < n - 2; ++i) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) { // Skip duplicates
+                int l = i + 1, r = n - 1, target = -nums[i];
+                while (l < r) {
+                    if (nums[l] + nums[r] == target) {
+                        ans.push_back({nums[i], nums[l], nums[r]});
+                        while (l < r && nums[l] == nums[l + 1]) l++; // Skip duplicates
+                        while (l < r && nums[r] == nums[r - 1]) r--; // Skip duplicates
+                        l++;
+                        r--;
+                    } else if (nums[l] + nums[r] < target) {
+                        l++;
+                    } else {
+                        r--;
+                    }
                 }
             }
         }
-        vector<vector<int>> ans(mainarr.begin(), mainarr.end());
+        
         return ans;
     }
 };
