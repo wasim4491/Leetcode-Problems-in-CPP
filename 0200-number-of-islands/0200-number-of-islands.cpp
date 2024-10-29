ce3cd6db0f2c4efd895bc52187b5b0d3b43f993c
wasim4491 <52147807+wasim4491@users.noreply.github.com>
2024-10-29 01:13:23 -0700

    Time: 36 ms (23.96%), Space: 22.1 MB (33.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,54 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int i, int j){
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        grid[i][j] = '0';
-        
-        if(i-1 >=0 && grid[i-1][j] == '1'){
-            dfs(grid, i-1, j);
+    
+    bool boundary(int m, int n, int i, int j){
+        if(i < 0 || j < 0 || i >= m || j >= n){
+            return false;
         }
-        if(i+1 < m && grid[i+1][j] == '1'){
-            dfs(grid, i+1, j);
-        }
-        if(j-1 >= 0 && grid[i][j-1] == '1'){
-            dfs(grid, i, j-1);
-        }
-        if(j+1 < n && grid[i][j+1] == '1'){
-            dfs(grid, i, j+1);
-        }
+        return true;
     }
+    
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         int count = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j] == '1'){
+        
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                
+                
+                if(!visited[i][j] && grid[i][j] == '1'){
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        pair<int, int> temp = q.front();
+                        q.pop();
+                        int a = temp.first;
+                        int b = temp.second;
+                        if(boundary(m, n, a+1, b) && !visited[a+1][b] && grid[a+1][b] == '1'){
+                            q.push({a+1, b});
+                            visited[a+1][b] = true;
+                        }
+                        if(boundary(m, n, a-1, b) && !visited[a-1][b] && grid[a-1][b] == '1'){
+                            q.push({a-1, b});
+                            visited[a-1][b] = true;
+                        }
+                        if(boundary(m, n, a, b+1) && !visited[a][b+1] && grid[a][b+1] == '1'){
+                            q.push({a, b+1});
+                            visited[a][b+1] = true;
+                        }
+                        if(boundary(m, n, a, b-1) && !visited[a][b-1] && grid[a][b-1] == '1'){
+                            q.push({a, b-1});
+                            visited[a][b-1] = true;
+                        }
+                    }
                     count++;
-                    dfs(grid, i, j);
                 }
             }
         }
+        
         return count;
     }
 };
